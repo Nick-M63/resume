5cbf041ba86de61bcdbf65f1f0bda98a80bbd19d
Nick-M63 <nickmookhoek@upcmaol.nl>
2026-01-14 12:33:05 +0100

    Created a layout

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,18 +1,15 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ImageComponent } from './image/image.component';
+import { MenuComponent } from './menu/menu.component';
+import { ContentComponent } from './content/content.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ImageComponent, MenuComponent, ContentComponent],
   templateUrl: './app.html',
-  styleUrl: './app.css',
+  styleUrls: ['./app.css'],
 })
 export class App {
   protected readonly title = signal('resume');
-
-  dummyFunction() {
-    const x = 1;
-    const Y = 5;
-    const z = 7;
-  }
 }
---
+++ src/app/image/image.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-image',
+  imports: [],
+  templateUrl: './image.component.html',
+  styleUrl: './image.component.css',
+})
+export class ImageComponent {}
