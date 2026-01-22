3c2bb5433b4a7e580a8394e919cfe6ee64c3dbe6
Nick-M63 <nickmookhoek@upcmaol.nl>
2026-01-22 12:53:03 +0100

    Started with the About component

--- src/app/content/about/about.component.ts
+++ src/app/content/about/about.component.ts
@@ -6,4 +6,7 @@ import { Component } from '@angular/core';
   templateUrl: './about.component.html',
   styleUrl: './about.component.css',
 })
-export class AboutComponent {}
+export class AboutComponent {
+  public age: string = '63 years';
+  public city: string = 'Vlaardingen';
+}
