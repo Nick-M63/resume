a04a2b850e9e0840f813af5dbd79fd1b11bde639
Nick-M63 <nickmookhoek@upcmaol.nl>
2026-01-28 12:46:03 +0100

    Started with the Skills Component

--- src/app/content/skills/skills.component.ts
+++ src/app/content/skills/skills.component.ts
@@ -1,4 +1,10 @@
-import { Component } from '@angular/core';
+import { OnInit, Component, ChangeDetectorRef, inject } from '@angular/core';
+
+export interface Skill {
+  name: string;
+  rating: number;
+  animatedRating: number;
+}
 
 @Component({
   selector: 'app-skills',
@@ -6,4 +12,23 @@ import { Component } from '@angular/core';
   templateUrl: './skills.component.html',
   styleUrl: './skills.component.css',
 })
-export class SkillsComponent {}
+export class SkillsComponent implements OnInit {
+  readonly #cdr = inject(ChangeDetectorRef);
+
+  skills: Skill[] = [
+    { name: 'Angular', rating: 80, animatedRating: 0 },
+    { name: 'HTML', rating: 85, animatedRating: 0 },
+    { name: 'CSS', rating: 70, animatedRating: 0 },
+    { name: 'TypeScript', rating: 75, animatedRating: 0 },
+    { name: 'Ionic', rating: 65, animatedRating: 0 },
+  ];
+
+  ngOnInit(): void {
+    this.skills.forEach((s, i) => {
+      setTimeout(() => {
+        s.animatedRating = s.rating;
+        this.#cdr.detectChanges();
+      }, i * 120);
+    });
+  }
+}
