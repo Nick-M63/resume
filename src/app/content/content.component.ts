a5e627c8933f492b28f1923c6682c4f84700fb8b
Nick-M63 <nickmookhoek@upcmaol.nl>
2026-02-06 11:01:51 +0100

    Added animation for language switch

--- src/app/content/content.component.ts
+++ src/app/content/content.component.ts
@@ -1,4 +1,4 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, effect, ChangeDetectorRef } from '@angular/core';
 import { RouterOutlet, RouterLinkActive, RouterLink } from '@angular/router';
 import { Language, LanguageService } from '../services/language.service';
 import { translations } from '../translations';
@@ -11,8 +11,32 @@ import { translations } from '../translations';
 })
 export class ContentComponent {
   readonly #languageService = inject(LanguageService);
+  readonly #cdr = inject(ChangeDetectorRef);
 
   public language = this.#languageService.language;
+  public languageAnimation = false;
+  #timer_1: any;
+  #timer_2: any;
+
+  constructor() {
+    effect(() => {
+      this.#languageService.language();
+
+      clearTimeout(this.#timer_1);
+      clearTimeout(this.#timer_2);
+
+      this.languageAnimation = false;
+
+      this.#timer_1 = setTimeout(() => {
+        this.languageAnimation = true;
+        this.#cdr.detectChanges();
+      }, 0);
+
+      this.#timer_2 = setTimeout(() => {
+        this.languageAnimation = false;
+      }, 300);
+    });
+  }
 
   translate(key: string): string | string[] {
     return translations[this.#languageService.language()][key] ?? key;
